3be5794264c354c2e4a2ca9dbad46882567858b3
Haris Sattar <harissattar41@gmail.com>
2016-01-30 23:23:51 -0800

    added description

--- hmw1/homework1.cpp
+++ hmw1/homework1.cpp
@@ -1,3 +1,8 @@
+/*
+ ^ Haris Sattar
+ * CSE 330
+ *
+ */
 #include <iostream>
 
 using namespace std;
@@ -5,10 +10,10 @@ using namespace std;
 float average(float d[], int n)
 {
 	float sum = 0;
-	
+
 	if (n == 0)
 		return 0;
-		
+
 	for (int i = 0; i < n; i++)
 		sum += d[i];
 
@@ -19,17 +24,17 @@ float variance(float d[], int n)
 {
 	float avrg = average(d, n);
 	float sum = 0;
-	
+
 	if (n == 0)
 		return 0;
-		
-	
+
+
 	for (int i = 0; i < n; i++) {
 		d[i] -= avrg;
 		d[i] *= d[i];
 		sum += d[i];
 	}
-	
+
 	return sum / (n - 1);
 }
 
@@ -37,11 +42,11 @@ int main()
 {
 	float value;
 	int numbers = 0;
-	float d[100];				
-	
+	float d[100];
+
 	for(int i = 0; i < 100 && cin >> value; i++, numbers++)
 		d[i] = value;
-	
-	cout << "average = " << average(d, numbers) << endl;	
-	cout << "variance = " << variance(d, numbers) << endl;	
+
+	cout << "average = " << average(d, numbers) << endl;
+	cout << "variance = " << variance(d, numbers) << endl;
 }
