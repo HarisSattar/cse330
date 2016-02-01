3bc350c30663a7ca115b307dc59b096d4027d44e
Haris Sattar <harissattar41@gmail.com>
2016-02-01 15:37:35 -0800

    added lab3

---
+++ lab3/String.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cstring>
+#include "String.h"
+
+using namespace std;
+
+String::String()
+{
+	size = 0;
+	buffer = 0;
+}
+
+String::String(const String & s)
+{
+	size = s.size;
+	buffer = new char[size];
+	for (int i = 0; i < size; i++)
+		buffer[i] = s.buffer[i];
+}
+
+String::String(const char* p)
+{
+	size = strlen(p);
+	buffer = new char[size];
+	
+	for (int i = 0; i < size; i++)
+		buffer[i] = p[i];
+}
+
+int String::length()
+{
+	return size;
+}
+
+char & String::operator[](int i)
+{
+	return buffer[i];
+}
+
+String String::operator=(const String& s)
+{
+	size = s.size;
+	buffer = new char[size];
+	
+	for (int i = 0; i < size; i++)
+		buffer[i] = s.buffer[i];
+		
+	return *this;
+}
+
+bool operator==(const String & left, const String & right)
+{
+	if (left.size != right.size)
+		return false;
+	
+	for (int i = 0; i < right.size; i++)
+		if (left.buffer[i] != right.buffer[i])
+			return false;
+	
+	return true;
+}
+
+String& operator+=(String & left, const String & right)
+{
+	String temp = left;
+	int size = left.size + right.size;
+	left.buffer = new char[size];
+	
+	int i;
+	for (i = 0; i < left.size; i++)
+		left.buffer[i] = temp.buffer[i];
+		
+	left.size = left.size + right.size;
+	
+	for (int j = 0; i < size; i++, j++)
+		left.buffer[i] = right.buffer[j];
+		
+	return left;
+}
+
+ostream & operator<<(ostream & os, const String & right)
+{
+	for (int i = 0; i < right.size; i++)
+		os << right.buffer[i];
+	
+	return os;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lab3/String.h
@@ -0,0 +1,36 @@
+#ifndef STRING_H
+#define STRING_H
+
+// String.h
+
+#include <iostream>
+
+using namespace std;
+
+class String {
+	int size;
+	char * buffer;
+public:
+	String();
+	String(const String &);
+	String(const char *);
+	
+	// other methods
+	int length();
+	char & operator[](int i);
+	String operator=(const String&);
+	
+	friend bool operator==(const String &, const String &);
+	friend String& operator+=(String &, const String &);
+	friend ostream & operator<<(ostream &, const String &);
+	
+	/*
+	~String();
+
+
+	friend bool operator<=(const String &, const String &);
+	friend bool operator<(const String &, const String &);
+	*/
+};
+
+#endif
