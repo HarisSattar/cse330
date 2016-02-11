b0415c3bc7118a1a72dbe747e47c70ed8935d8f5
Haris Sattar <harissattar41@gmail.com>
2016-02-11 10:00:15 +0000

    finished lab4

--- lab4/Vector.h
+++ lab4/Vector.h
@@ -14,8 +14,8 @@ public:
 
 	Vector();
 	Vector(unsigned int size);
-	Vector(unsigned int size, const T & initial);
-	Vector(const Vector<T> & v);           // copy constructor
+	Vector(unsigned int size, const T& initial);
+	Vector(const Vector<T>& v);           // copy constructor
 	~Vector();
 
 	unsigned int capacity() const;         // return capacity of vector (in elements)
@@ -24,44 +24,40 @@ public:
 
 	iterator begin();                      // return an iterator pointing to the first element
 	iterator end();                        // return an iterator pointing to one past the last element
-	T & front();                           // return a reference to the first element
-	T & back();                            // return a reference to the last element
-	void push_back(const T & value);       // add a new element
+	T& front();                           // return a reference to the first element
+	T& back();                            // return a reference to the last element
+	void push_back(const T& value);       // add a new element
 	void pop_back();                       // remove the last element
 
 	void reserve(unsigned int capacity);   // adjust capacity
 	void resize(unsigned int size);        // adjust size
 
-	T & operator[](unsigned int index);    // return reference to numbered element
-	Vector<T> & operator=(const Vector<T> &);
-	bool operator==(const Vector<T> &);
+	T& operator[](unsigned int index);    // return reference to numbered element
+	Vector<T>& operator=(const Vector<T>&);
+	bool operator==(const Vector<T>&);
 
 private:
 	unsigned int my_size;
 	unsigned int my_capacity;
-	T * buffer;
+	T* buffer;
 };
-
-// Two sample methods follow
 
 template <class T>
 Vector<T>::Vector()
 {
-	my_size = 0;
-	my_capacity = 0;
 	buffer = 0;
+	resize(0);
 }
 
 template <class T>
 Vector<T>::Vector(unsigned int size)
 {
-	my_size = size;
-	my_capacity = my_size;
-	buffer = new T[my_size];
+	buffer = 0;
+	resize(size);
 }
 
 template <class T>
-Vector<T>::Vector(unsigned int size, const T & initial)
+Vector<T>::Vector(unsigned int size, const T& initial)
 {
 	my_size = size;
 	my_capacity = my_size;
@@ -72,7 +68,7 @@ Vector<T>::Vector(unsigned int size, const T & initial)
 }
 
 template <class T>
-Vector<T>::Vector(const Vector<T> & v)
+Vector<T>::Vector(const Vector<T>& v)
 {
 	my_size = v.size();
 	my_capacity = v.capacity();
@@ -123,6 +119,11 @@ bool Vector<T>::empty() const
 template <class T>
 void Vector<T>::reserve(unsigned int capacity)
 {
+	if (buffer == 0) {
+		my_size = 0;
+		my_capacity = 0;
+	}
+
 	if (capacity <= my_capacity)
 		return;
 		
@@ -153,7 +154,7 @@ void Vector<T>::pop_back()
 }
 
 template <class T>
-void Vector<T>::push_back(const T & value)
+void Vector<T>::push_back(const T& value)
 {
 	if (my_size >= my_capacity)
 		reserve(my_capacity + 5);
@@ -163,7 +164,7 @@ void Vector<T>::push_back(const T & value)
 }
 
 template <class T>
-bool Vector<T>::operator==(const Vector<T> & right)
+bool Vector<T>::operator==(const Vector<T>& right)
 {
 	if (size != right.size)
 		return false;
@@ -177,13 +178,13 @@ bool Vector<T>::operator==(const Vector<T> & right)
 }
 
 template <class T>
-T & Vector<T>::operator[](unsigned int index)
+T& Vector<T>::operator[](unsigned int index)
 {
 	return buffer[index];
 }
 
 template <class T>
-Vector<T> & Vector<T>::operator=(const Vector<T> & right)
+Vector<T>& Vector<T>::operator=(const Vector<T>& right)
 {
 	delete [] buffer;
 	my_size = right.size();
@@ -192,6 +193,18 @@ Vector<T> & Vector<T>::operator=(const Vector<T> & right)
 	
 	for (int i = 0; i < my_size; i++)
 		buffer[i] = right.buffer[i];
+}
+
+template <class T>
+T& Vector<T>::front()
+{
+	return buffer[0];	
+}
+
+template <class T>
+T& Vector<T>::back()
+{
+	return buffer[my_size - 1];
 }
 
 #endif
